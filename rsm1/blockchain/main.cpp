ce9f4877443b5fa493768e533e2a18f607debf35
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-10-03 10:59:05 +0200

    Update main.cpp

--- rsm1/blockchain/main.cpp
+++ rsm1/blockchain/main.cpp
@@ -1,4 +1,4 @@
-﻿// redstone.multichain.cpp : Defines the entry point for the application.
+// redstone.multichain.cpp : Defines the entry point for the application.
 //RedStone Main.cpp
 #include <iostream>
 #include "sha256.h"
@@ -7,23 +7,40 @@
 using std::string;
 using std::cout;
 using std::endl;
+Welcome = "Welcome to the redstone client";
+Nodestarting = "Your node is starting please wait";
+Error = "Something wrong. if this keeps happening again contact us on discord: red-stone.me";
+Syncing = "Your node is syncing please wait!";
+Pnf = "Peers not found";  
+Pf = "Peers Found!"
+int main(){
+    std::cout << Welcome << endl;
+   
+    
 
+
+}
 int main(int argc, char* argv[])
 {
     string input = "grape";
     string output1 = sha256(input);
 
     cout << "sha256('" << input << "'):" << output1 << endl;
+    //return 0;
+    int count;
+    while(true)
+    {
+       Blockchain bChain = Blockchain(); //Loop to mine block every 10 sec loop will coint block that block that counted will be added to the cahin i think
+       Block block = Block(count, "Block Data");
+       cout << "Mining block" << endl;
+       bChain.AddBlock(block);
+       count +=1; 
+        // this does not print every 10 secconds, this prints as fast as it can. You need to implment a non blocking delay (by checking timestamp diffrence and only calling
+        //cout when the diffrence is over 10 secconds)
+       cout << block << endl; // print every 10 second 
+    }
     return 0;
 }
-//int count;
- //   while(true)
- //   {
- //      Blockchain bChain = Blockchain(); //Loop to mine block every 10 sec loop will coint block that block that counted will be added to the cahin i think
-  //     block = cout << "Mining block" << endl; bChain.AddBlock(Block(count, "Block Data"));
-  //     count +=1; 
-  //     cout << block << endl; // print every 10 second 
-   // }
 //int main() {
  //   Blockchain bChain = Blockchain();
 
